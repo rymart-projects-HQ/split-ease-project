523da68f94493ec327c42450ed7bd0ca81a8da3e
Rcasas14 <reymartcasas123@gmail.com>
2026-01-07 04:17:40 +0800

    this should not work

--- app/app/app.vue
+++ app/app/app.vue
@@ -1,3 +1,8 @@
+<script setup lang="ts">
+import { ref, computed } from "vue";
+
+</script>
+
 <template>
   <div>
     <NuxtRouteAnnouncer />
--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -2,7 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  
   modules: ["@nuxt/eslint"],
 
   eslint: {
