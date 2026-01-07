2a4a1bb678c63f21bf1f9788d97eb8e872a6bd62
Rcasas14 <reymartcasas123@gmail.com>
2026-01-08 01:27:48 +0800

    added some files

--- app/app/app.vue
+++ app/app/app.vue
@@ -1,6 +1,9 @@
+<script setup>
+import "~/assets/main.css";
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
