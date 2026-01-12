2e030d761563a954717e81cbaa8d41e53abf8290
Reymart <reymartcasas123@gmail.com>
2026-01-13 05:17:42 +0800

    style: fix directories pathing, and align lint format

---
+++ app/error.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <h1 class="text-3xl font-bold underline">
+      Sample Text
+    </h1>
+  </div>
+</template>
---
+++ app/pages/login/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: "app",
+});
+</script>
+
+<template>
+  <div>
+    <h1 class="text-3xl font-bold underline">
+      Sample Text
+    </h1>
+  </div>
+</template>
